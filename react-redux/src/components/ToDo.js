1165aec2c83a55f89e5949becc3532d8b163eadb
sunm-in <sunm309@naver.com>
2021-08-12 00:37:19 +0900

    🎨 Learning React - Redux Toolkit

--- react-redux/src/components/ToDo.js
+++ react-redux/src/components/ToDo.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { actionCreators } from '../store';
+import { remove } from '../store';
 import { Link } from 'react-router-dom';
 
 const ToDo = ({ text, onBtnClick, id }) => {
   return (
     <li>
-      <Link to={`/${id}`}>
-        {text} <button onClick={onBtnClick}>DEL</button>
-      </Link>
+      <Link to={`/${id}`}>{text}</Link>
+      <button onClick={onBtnClick}>DEL</button>
     </li>
   );
 };
 
 function mapDispatchToProps(dispatch, ownProps) {
-  return { onBtnClick: () => dispatch(actionCreators.deleteToDo(ownProps.id)) };
+  return { onBtnClick: () => dispatch(remove(ownProps.id)) };
 }
 
 export default connect(null, mapDispatchToProps)(ToDo);
--- react-redux/src/store.js
+++ react-redux/src/store.js
@@ -1,42 +1,46 @@
-import { createStore } from 'redux';
-
-const ADD = 'ADD';
-const DELETE = 'DELETE';
-
-// action creator
-const addToDo = (text) => {
-  return {
-    type: ADD,
-    text,
-  };
-};
-
-const deleteToDo = (id) => {
-  return {
-    type: DELETE,
-    id: parseInt(id),
-  };
-};
+// import { createStore } from 'redux';
+import {
+  configureStore,
+  // createAction,
+  // createReducer,
+  createSlice,
+} from '@reduxjs/toolkit';
 
 // getState -> state 전달
 // dispatch -> store 혹은 reducer에 메세지를 전달
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE:
-      return state.filter((toDo) => toDo.id !== action.id);
-    default:
-      return state;
-  }
-};
+/*
+// action creator
+const addToDo = createAction('ADD');
+const deleteToDo = createAction('DELETE');
 
-const store = createStore(reducer);
+// reducer
+// createReducer를 사용하면 state를 수정할 때 쉽게 만들어준다.
+const reducer = createReducer([], {
+  [addToDo]: (state, action) => {
+    // state를 mutate 하면 아무것도 리턴하지 않는다.
+    state.push({ text: action.payload, id: Date.now() });
+  },
+  [deleteToDo]: (state, action) =>
+    // state를 mutate(새로운 형태로 변형)하지 않으면 새로운 state를 리턴해준다. -> ✅ 뭔가를 리턴할 땐 꼭 새로운 state여야 한다.
+    state.filter((toDo) => toDo.id !== action.payload),
+}); 
+*/
 
-export const actionCreators = {
-  addToDo,
-  deleteToDo,
-};
+const toDos = createSlice({
+  name: 'toDosReducer',
+  initialState: [],
+  reducers: {
+    add: (state, action) => {
+      state.push({ text: action.payload, id: Date.now() });
+    },
+    remove: (state, action) =>
+      state.filter((toDo) => toDo.id !== action.payload),
+  },
+});
+
+const store = configureStore({ reducer: toDos.reducer });
+
+export const { add, remove } = toDos.actions;
 
 export default store;
