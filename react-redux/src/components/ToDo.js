4c650eae8654c9482b061aed32133f316584d38f
sunm-in <sunm309@naver.com>
2021-08-11 14:22:04 +0900

    🎨 Learning React Redux

--- react-redux/src/components/ToDo.js
+++ react-redux/src/components/ToDo.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { actionCreators } from '../store';
+import { Link } from 'react-router-dom';
 
-const ToDo = ({ text, onBtnClick }) => {
+const ToDo = ({ text, onBtnClick, id }) => {
   return (
     <li>
-      {text} <button onClick={onBtnClick}>DEL</button>
+      <Link to={`/${id}`}>
+        {text} <button onClick={onBtnClick}>DEL</button>
+      </Link>
     </li>
   );
 };
