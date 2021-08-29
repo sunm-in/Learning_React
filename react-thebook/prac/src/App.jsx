2b6646fa5abe589bcf18f196ea5908b3427b2533
sunm-in <sunm309@naver.com>
2021-08-29 22:56:29 +0900

    📝 Learning React - 리액트를 다루는 기술

--- react-thebook/prac/src/App.jsx
+++ react-thebook/prac/src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import EventPractice from './components/EventPractice';
+import IterationSample from './components/IterationSample';
 
 function App() {
-  return <EventPractice />;
+  return <IterationSample />;
 }
 
 export default App;
