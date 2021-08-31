7473c95e3a0e03f01bfabe474f4383575445692c
sunm-in <sunm309@naver.com>
2021-08-31 23:09:37 +0900

    📝 Learning React - useReducer

--- react-thebook/hooks-tutorial/src/App.jsx
+++ react-thebook/hooks-tutorial/src/App.jsx
@@ -2,21 +2,7 @@ import React, { useState } from 'react';
 import Info from './components/Info';
 
 const App = () => {
-  const [visible, setVisible] = useState(false);
-
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}
-      >
-        {visible ? '숨기기' : '보이기'}
-      </button>
-      <hr />
-      {visible && <Info />}
-    </div>
-  );
+  return <Info />;
 };
 
 export default App;
