aae53c8d06847b8d685632b4662d032560c3769c
sunm-in <sunm309@naver.com>
2021-09-02 22:30:31 +0900

    🔨 input값 저장하기

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -4,12 +4,24 @@ import './App.css';
 
 // component
 import Modal from './components/Modal';
+import Profile from './components/Profile';
 
 function App() {
   const [title, setTitle] = useState(['여행지 추천', '카페 추천', '맛집 추천']);
   const [like, setLike] = useState([0, 0, 0]);
   const [modal, setModal] = useState(false);
   const [modalTitle, setModalTitle] = useState(0);
+  const [input, setInput] = useState('');
+
+  const inputValue = (e) => {
+    setInput(e.target.value);
+  };
+
+  const addTitleBtn = () => {
+    let arrayTitle = [...title];
+    arrayTitle.unshift(input);
+    setTitle(arrayTitle);
+  };
 
   const changeTitle = () => {
     let newTitle = [...title];
@@ -53,6 +65,11 @@ function App() {
         );
       })}
 
+      <div className="publish">
+        <input onChange={inputValue} />
+        <button onClick={addTitleBtn}>저장</button>
+      </div>
+
       <button
         onClick={() => {
           setModal(!modal);
@@ -60,6 +77,8 @@ function App() {
       >
         열고닫기
       </button>
+
+      <Profile />
 
       {modal === true ? <Modal title={title} modalTitle={modalTitle} /> : null}
     </div>
