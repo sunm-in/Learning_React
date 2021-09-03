345f99f38429d5f9744e8c1c427980f075292a99
sunm-in <sunm309@naver.com>
2021-09-03 20:54:17 +0900

    📝 Learning React - styling

---
+++ react-thebook/styling-react/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import StyledComponent from './components/StyledComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <StyledComponent />
+    </div>
+  );
+}
+
+export default App;
