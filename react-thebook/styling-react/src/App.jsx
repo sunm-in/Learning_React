11c7106bad63b958fa17010e4c63978b902c91f7
sunm-in <sunm309@naver.com>
2021-09-03 18:07:14 +0900

    📝 Learning React - styling

---
+++ react-thebook/styling-react/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SassComponent from './components/SassComponent';
+
+function App() {
+  return (
+    <div>
+      <SassComponent />
+    </div>
+  );
+}
+
+export default App;
