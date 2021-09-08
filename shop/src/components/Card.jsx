e1a56be6a393b3744ee1a6421972a2db0d8177db
sunm-in <sunm309@naver.com>
2021-09-08 23:35:29 +0900

    🔨 Card components create

---
+++ shop/src/components/Card.jsx
@@ -0,0 +1,16 @@
+// library
+import React from 'react';
+
+const PlacePost = (props) => {
+  return (
+    <div className="col-md-4">
+      <img alt="" src={props.place.image} width="100%" />
+      <h4>{props.place.title}</h4>
+      <p>
+        {props.place.content} & {props.place.price}
+      </p>
+    </div>
+  );
+};
+
+export default PlacePost;
