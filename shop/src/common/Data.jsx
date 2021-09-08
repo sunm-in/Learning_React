ed3fa75feaa44125e20ce57a53be577b5c61383e
sunm-in <sunm309@naver.com>
2021-09-08 23:35:40 +0900

    🔨 Data common create

---
+++ shop/src/common/Data.jsx
@@ -0,0 +1,30 @@
+const Data = [
+  {
+    id: 0,
+    title: 'White and Black',
+    image:
+      'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMDEwMDJfMjEx%2FMDAxNjAxNjEyMjEyMjI1.p7CRey9DouydgtCV0oUlFS072ke2Ad_sr1NAL8L4Er8g.8ZTs8eYrWqktEJUcvb1hK_EGe4SVy2ut5tdEMASIDpAg.JPEG.jascoco%2F1601612210000.jpg&type=sc960_832',
+    content: 'Born in France',
+    price: 120000,
+  },
+
+  {
+    id: 1,
+    title: 'Red Knit',
+    image:
+      'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMTA3MzBfMjgw%2FMDAxNjI3NjI3MTU5MDY0.FLDk65Tf21UkCJPtIuccTAHtj9ByPvNbtizSjPwVWEIg.hGZQx-U35zr5Ds_vPFAg_aMXTMIB5KyWeLXC9bUaTlYg.JPEG.charlotteblue23%2FDSC03175.JPG&type=sc960_832',
+    content: 'Born in Seoul',
+    price: 110000,
+  },
+
+  {
+    id: 2,
+    title: 'Grey Yordan',
+    image:
+      'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMTA2MThfMjM3%2FMDAxNjI0MDA1NzA4NjEx.bpCCGkwn7TuXLK2PiuCWS09dWtdVtzNi52cGskWU0kcg.5WYZK0Gebv6ZORwtR9lHzJ_RbAJ_wLL-RJTTgkQtDoAg.JPEG.bryn88%2F%25B1%25D7%25B6%25F3%25B5%25A5%25C0%25CC%25BC%25C7%25C7%25CA%25C5%25CD%25C0%25FB%25BF%25EB.jpg&type=sc960_832',
+    content: 'Born in the States',
+    price: 130000,
+  },
+];
+
+export default Data;
