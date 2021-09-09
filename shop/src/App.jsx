95a2bce568b6d0bd30240f95312483b4a3ac1984
sunm-in <sunm309@naver.com>
2021-09-09 22:51:03 +0900

    🔨 페이지 연결

--- shop/src/App.jsx
+++ shop/src/App.jsx
@@ -1,53 +1,29 @@
 /* eslint-disable */
 // library
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import React from 'react';
 
-// component
-import Card from './components/Card';
-
-// bootstrap
-import { Navbar, Container, Nav } from 'react-bootstrap';
+// route
+import { Route, Switch } from 'react-router-dom';
 
 // style
 import './App.css';
 
-// data
-import Data from './common/Data';
+// component
+import { Header } from './components';
+
+// page
+import { Main, Detail } from './pages';
 
 function App() {
-  const [place, setPlace] = useState(Data);
-
   return (
     <div className="App">
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">🐶 Lucky 🐶</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-      <Jumbotron />
-      <div className="container">
-        <div className="row">
-          {place.map((place, i) => {
-            return <Card key={i} place={place} />;
-          })}
-        </div>
-      </div>
+      <Header />
+      <Switch>
+        <Route path="/" component={Main} exact />
+        <Route path="/detail/:id" component={Detail} exact />
+      </Switch>
     </div>
   );
 }
-
-const Jumbotron = styled.div`
-  width: 100%;
-  height: 350px;
-  background-image: url('https://d2u3dcdbebyaiu.cloudfront.net/uploads/atch_img/621/2f34602ed32553ee5809a70a1321aefb.jpeg');
-  background-size: cover;
-  background-position: center;
-`;
 
 export default App;
