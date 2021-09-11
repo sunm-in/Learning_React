e031139695dadd2d97f24e0d2a3f27df457e8058
sunm-in <sunm309@naver.com>
2021-09-11 21:52:18 +0900

    🔨 Learning React - axios

--- shop/src/pages/Main.jsx
+++ shop/src/pages/Main.jsx
@@ -1,5 +1,6 @@
 // library
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 // component
 import { Card } from '../components';
@@ -7,20 +8,66 @@ import { Card } from '../components';
 // data
 import Data from '../common/Data';
 
+// axios
+import axios from 'axios';
+
 const Main = () => {
   const [place, setPlace] = useState(Data);
+  console.log(place);
+
+  const [loading, setLoading] = useState(false);
 
   return (
     <>
       <div className="container">
         <div className="row">
           {place.map((place, i) => {
-            return <Card key={i} place={place} />;
+            return <Card key={i} place={place} i={i} />;
           })}
         </div>
+
+        {loading === true ? (
+          <LoadingBoxStyle>
+            <p>로딩중입니다...</p>
+          </LoadingBoxStyle>
+        ) : null}
+
+        <button
+          className="btn btn-primary"
+          onClick={() => {
+            setLoading(true);
+
+            // axios.post('서버 URL', { id: 'react', pw: 1234 });
+
+            axios
+              .get('https://codingapple1.github.io/shop/data2.json')
+              .then((res) => {
+                setLoading(false);
+                console.log(res.data);
+                setPlace([...place, ...res.data]);
+              })
+              .catch(() => {
+                console.error('GET 요청 실패');
+              });
+          }}
+        >
+          더보기
+        </button>
       </div>
     </>
   );
 };
+
+const LoadingBoxStyle = styled.div`
+  background: whitesmoke;
+  padding: 16px;
+  max-width: 500px;
+  width: 100%;
+  margin: 2% auto;
+
+  & p {
+    margin-bottom: 0;
+  }
+`;
 
 export default Main;
