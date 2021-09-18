517d74ecd5a71a3e3a29a7365c4ebad620111249
sunm-in <sunm309@naver.com>
2021-09-18 23:03:04 +0900

    🔨 tabMenu, animation 적용

--- shop/src/components/CardDetail.jsx
+++ shop/src/components/CardDetail.jsx
@@ -3,6 +3,10 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import styled from 'styled-components';
+import { CSSTransition } from 'react-transition-group';
+
+// bootstrap
+import { Nav } from 'react-bootstrap';
 
 // component
 import { Info } from './index';
@@ -23,11 +27,12 @@ const CardDetail = (props) => {
   const history = useHistory();
   const { id } = useParams();
   const _place = props.place.find((x) => x.id === parseInt(id));
-
   const [alert, setAlert] = useState(true);
   const [text, setText] = useState('');
-
   const [stock, setStock] = useState([10, 11, 12]);
+
+  const [tab, setTab] = useState(0); // 지금 누른 번호
+  const [aniSwitch, setAniSwitch] = useState(false);
 
   useEffect(() => {
     let timer = setTimeout(() => {
@@ -88,8 +93,56 @@ const CardDetail = (props) => {
           </button>
         </div>
       </div>
+
+      <Nav
+        style={{ marginBottom: '10%' }}
+        className="mt-5"
+        variant="tabs"
+        defaultActiveKey="link-0"
+      >
+        <Nav.Item>
+          <Nav.Link
+            eventKey="link-0"
+            onClick={() => {
+              setAniSwitch(false);
+              setTab(0);
+            }}
+          >
+            Active
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            eventKey="link-1"
+            onClick={() => {
+              setAniSwitch(false);
+              setTab(1);
+            }}
+          >
+            Option 2
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+
+      <CSSTransition in={aniSwitch} classNames="wow" timeout={500}>
+        <TabContent tab={tab} setAniSwitch={setAniSwitch} />
+      </CSSTransition>
     </div>
   );
 };
+
+function TabContent(props) {
+  useEffect(() => {
+    props.setAniSwitch(true);
+  });
+
+  if (props.tab === 0) {
+    return <div>0번째 내용</div>;
+  } else if (props.tab === 1) {
+    return <div>1번째 내용</div>;
+  } else if (props.tab === 2) {
+    <div>2번째 내용</div>;
+  }
+}
 
 export default CardDetail;
