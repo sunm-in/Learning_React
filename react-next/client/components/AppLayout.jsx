16b8b60a266c1ecf7181c798e3a39a50d6f6723c
sunmin <sunm309@naver.com>
2022-01-04 21:31:57 +0900

    🔧 상단 메뉴 antd 적용

---
+++ react-next/client/components/AppLayout.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'next/link';
+import { Menu } from 'antd';
+
+const AppLayout = ({ children }) => {
+  return (
+    <div>
+      <Menu mode='horizontal'>
+        <Menu.Item>
+          <Link href='/'>
+            <a>메인페이지</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href='/profile'>
+            <a>프로필</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href='/signup'>
+            <a>회원가입</a>
+          </Link>
+        </Menu.Item>
+      </Menu>
+
+      {children}
+    </div>
+  );
+};
+
+AppLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default AppLayout;
