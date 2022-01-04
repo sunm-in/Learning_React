e68b24f1b284b3afcd1d1d6a404237da72438abf
sunmin <sunm309@naver.com>
2022-01-04 21:31:42 +0900

    🔧 페이지 생성

---
+++ react-next/client/pages/profile.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Head from 'next/head';
+import AppLayout from '../components/AppLayout';
+
+const Profile = () => {
+  return (
+    <>
+      <Head>
+        <title>내 프로필 | Sunmin</title>
+      </Head>
+      <AppLayout>프로필</AppLayout>
+    </>
+  );
+};
+
+export default Profile;
