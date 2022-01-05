8e7016279ad322ca2f1ddcfa1a6cf5ddbf8a1916
sunmin <sunm309@naver.com>
2022-01-05 21:48:00 +0900

    💄 profile - page

--- react-next/client/pages/profile.jsx
+++ react-next/client/pages/profile.jsx
@@ -1,14 +1,28 @@
+// library
 import React from 'react';
+
+// head
 import Head from 'next/head';
+
+// components
 import AppLayout from '../components/AppLayout';
+import NicknameEditForm from '../components/NicknameEditForm';
+import FollowList from '../components/FollowList';
 
 const Profile = () => {
+  const followerList = [{ nickname: '이선민' }, { nickname: '선민' }, { nickname: '민' }];
+  const followingList = [{ nickname: '이선민' }, { nickname: '선민' }, { nickname: '민' }];
+
   return (
     <>
       <Head>
         <title>내 프로필 | Sunmin</title>
       </Head>
-      <AppLayout>프로필</AppLayout>
+      <AppLayout>
+        <NicknameEditForm />
+        <FollowList header='팔로잉 목록' data={followingList} />
+        <FollowList header='팔로워 목록' data={followerList} />
+      </AppLayout>
     </>
   );
 };
