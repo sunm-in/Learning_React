cfe7d5f82c250638f3b459a2da8b7b6e09528e29
sunmin <sunm309@naver.com>
2022-01-05 23:00:57 +0900

    💄 signup - page

--- react-next/client/pages/signup.jsx
+++ react-next/client/pages/signup.jsx
@@ -1,15 +1,107 @@
-import React from 'react';
+// library
+import React, { useCallback, useState } from 'react';
+import styled from 'styled-components';
+
+// head
 import Head from 'next/head';
+
+// hooks
+import useInput from '../hooks/useInput';
+
+// components
 import AppLayout from '../components/AppLayout';
 
+// antd
+import { Form, Input, Checkbox, Button } from 'antd';
+
+const ErrorMessage = styled.div`
+  color: red;
+`;
+
 const SignUp = () => {
+  const [id, onChangeId] = useInput('');
+  const [nickname, onChangeNickname] = useInput('');
+  const [password, onChangePassword] = useInput('');
+
+  const [passwordCheck, setPasswordCheck] = useState('');
+  const [passwordError, setPasswordError] = useState(false);
+  const onChangePasswordCheck = useCallback(
+    (e) => {
+      setPasswordCheck(e.target.value);
+      setPasswordError(e.target.value !== password);
+    },
+    [password]
+  );
+
+  const [term, setTerm] = useState('');
+  const [termError, setTermError] = useState(false);
+  const onChangeTerm = useCallback((e) => {
+    setTerm(e.target.checked);
+    setTermError(false);
+  }, []);
+
+  const onSubmit = useCallback(() => {
+    if (password !== passwordCheck) {
+      return setPasswordError(true);
+    }
+    if (!term) {
+      return setTermError(true);
+    }
+    console.log(id, nickname, password);
+  }, [password, passwordCheck, term]);
+
   return (
-    <>
+    <AppLayout>
       <Head>
         <title>회원가입 | Sunmin</title>
       </Head>
-      <AppLayout>회원가입 페이지</AppLayout>
-    </>
+      <Form onFinish={onSubmit}>
+        <div>
+          <label htmlFor='user-id'>아이디</label>
+          <br />
+          <Input name='user-id' value={id} required onChange={onChangeId} />
+        </div>
+        <div>
+          <label htmlFor='user-nick'>닉네임</label>
+          <br />
+          <Input name='user-nick' value={nickname} required onChange={onChangeNickname} />
+        </div>
+        <div>
+          <label htmlFor='user-password'>비밀번호</label>
+          <br />
+          <Input
+            name='user-password'
+            type='password'
+            value={password}
+            required
+            onChange={onChangePassword}
+          />
+        </div>
+        <div>
+          <label htmlFor='user-password-check'>비밀번호 체크</label>
+          <br />
+          <Input
+            name='user-password-check'
+            type='password'
+            value={passwordCheck}
+            required
+            onChange={onChangePasswordCheck}
+          />
+          {passwordError && <ErrorMessage>비밀번호가 일치하지 않습니다.</ErrorMessage>}
+        </div>
+        <div>
+          <Checkbox name='user-term' checked={term} onChange={onChangeTerm}>
+            약관에 동의해 주세요.
+          </Checkbox>
+          {termError && <ErrorMessage>약관에 동의하셔야 합니다.</ErrorMessage>}
+        </div>
+        <div style={{ marginTop: '10px' }}>
+          <Button type='primary' htmlType='submit'>
+            가입하기
+          </Button>
+        </div>
+      </Form>
+    </AppLayout>
   );
 };
 
