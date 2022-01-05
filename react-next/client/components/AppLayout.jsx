bb260df06c86565194996bce79322d8fbeffe0dc
sunmin <sunm309@naver.com>
2022-01-05 16:15:49 +0900

    🔨 검색창 추가, 반응형 적용

--- react-next/client/components/AppLayout.jsx
+++ react-next/client/components/AppLayout.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+// library
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+
+// link
 import Link from 'next/link';
-import { Menu } from 'antd';
+
+// components
+import UserProfile from './UserProfile';
+import LoginForm from './LoginForm';
+
+// antd
+import { Menu, Input, Row, Col } from 'antd';
 
 const AppLayout = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div>
       <Menu mode='horizontal'>
@@ -18,13 +29,27 @@ const AppLayout = ({ children }) => {
           </Link>
         </Menu.Item>
         <Menu.Item>
+          <Input.Search enterButton style={{ verticalAlign: 'middle' }} />
+        </Menu.Item>
+        <Menu.Item>
           <Link href='/signup'>
             <a>회원가입</a>
           </Link>
         </Menu.Item>
       </Menu>
-
-      {children}
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          {isLoggedIn ? <UserProfile /> : <LoginForm />}
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a href='https://github.com/sunm-in' target='_blank' rel='noreferrer noopener'>
+            Made by Sunmin
+          </a>
+        </Col>
+      </Row>
     </div>
   );
 };
