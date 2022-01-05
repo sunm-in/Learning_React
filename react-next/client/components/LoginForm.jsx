5747faf3d5aa28203d1bd06a0e1d5d8644396ee1
sunmin <sunm309@naver.com>
2022-01-05 16:15:02 +0900

    🔨 로그인 폼 생성

---
+++ react-next/client/components/LoginForm.jsx
@@ -0,0 +1,52 @@
+// library
+import React, { useCallback, useState } from 'react';
+
+// link
+import Link from 'next/link';
+
+// antd
+import { Button, Form, Input } from 'antd';
+
+const LoginForm = () => {
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onChangeId = useCallback((e) => {
+    setId(e.target.value);
+  }, []);
+
+  const onChangePassword = useCallback((e) => {
+    setPassword(e.target.value);
+  }, []);
+
+  return (
+    <Form>
+      <div>
+        <label htmlFor='user-id'>아이디</label>
+        <br />
+        <Input name='user-id' value={id} onChange={onChangeId} required />
+      </div>
+      <div>
+        <label htmlFor='user-password'>비밀번호</label>
+        <br />
+        <Input
+          name='user-password'
+          value={password}
+          onChange={onChangePassword}
+          type='password'
+          required
+        />
+      </div>
+      <div>
+        <Button>로그인</Button>
+        <Link href='/signup'>
+          <a>
+            <Button>회원가입</Button>
+          </a>
+        </Link>
+      </div>
+    </Form>
+  );
+};
+
+export default LoginForm;
