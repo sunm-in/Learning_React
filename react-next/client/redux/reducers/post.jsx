e77cd3ac314806cecf11ae3ab1b41922b8afe03c
sunmin <sunm309@naver.com>
2022-01-06 16:13:32 +0900

    🔨 wrapper, redux 설정 및 적용

---
+++ react-next/client/redux/reducers/post.jsx
@@ -0,0 +1,71 @@
+export const initialState = {
+  mainPosts: [
+    {
+      id: 1,
+      User: {
+        id: 1,
+        nickname: 'nickname1',
+      },
+      content: '첫 번째 게시글 #test',
+      Images: [
+        {
+          src: 'https://joyvancouver.com/wp-content/uploads/2021/09/iphone13_210914.jpg',
+        },
+        {
+          src: 'https://betanews.net/imagedb/thumb/2021/1001/33740d8e.jpg',
+        },
+        {
+          src: 'https://s3.orbi.kr/data/file/united2/edb6f799d25e48d5b845037231de8163.jpg',
+        },
+      ],
+      Comments: [
+        {
+          User: {
+            nickname: 'nick1',
+          },
+          content: 'comment content 1',
+        },
+        {
+          User: {
+            nickname: 'nick2',
+          },
+          content: 'comment content2',
+        },
+      ],
+    },
+  ],
+  imagePaths: [],
+  postAdded: false,
+};
+
+const ADD_POST = 'ADD_POST';
+
+export const addPost = {
+  type: ADD_POST,
+};
+
+const dummyPost = {
+  id: 2,
+  content: 'dummy data',
+  User: {
+    id: 1,
+    nickname: 'nickname1',
+  },
+  Images: [],
+  Comments: [],
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_POST:
+      return {
+        ...state,
+        mainPosts: [dummyPost, ...state.mainPosts],
+        postAdded: true,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
