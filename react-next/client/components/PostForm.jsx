f5735a218b23494580340f704170b2a977bb4874
sunmin <sunm309@naver.com>
2022-01-06 16:10:09 +0900

    💄 PostForm, PostCard - components

---
+++ react-next/client/components/PostForm.jsx
@@ -0,0 +1,59 @@
+// library
+import React, { useCallback, useState, useRef } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+// antd
+import { Form, Input, Button } from 'antd';
+
+// redux
+import { addPost } from '../redux/reducers/post';
+
+const PostForm = () => {
+  const dispatch = useDispatch();
+  const [text, setText] = useState('');
+  const { imagePaths } = useSelector((state) => state.post);
+  const imageInput = useRef();
+
+  const onChangeText = useCallback((e) => {
+    setText(e.target.value);
+  }, []);
+
+  const onClickImageUpload = useCallback(() => {
+    imageInput.current.click();
+  }, [imageInput.current]);
+
+  const onSubmit = useCallback(() => {
+    dispatch(addPost);
+    setText('');
+  }, []);
+
+  return (
+    <Form style={{ margin: '10px 0 20px' }} encType='multipart/form-data' onFinish={onSubmit}>
+      <Input.TextArea
+        value={text}
+        onChange={onChangeText}
+        maxLength={140}
+        placeholder='내용을 입력해 주세요.'
+      />
+      <div>
+        <input type='file' multiple hidden ref={imageInput} />
+        <Button onClick={onClickImageUpload}>이미지 업로드</Button>
+        <Button type='primary' style={{ float: 'right' }} htmlType='submit'>
+          테스트
+        </Button>
+      </div>
+      <div>
+        {imagePaths.map((v) => (
+          <div key={v} style={{ display: 'inline-block' }}>
+            <img src={v} alt={v} style={{ width: '200px' }} />
+            <div>
+              <Button>제거</Button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </Form>
+  );
+};
+
+export default PostForm;
