178d3b40d33c20d4a5aae12b3867901af2d364da
sunmin <sunm309@naver.com>
2022-01-09 22:08:08 +0900

    ♻️ description 수정

--- react-next/client/components/PostCard.jsx
+++ react-next/client/components/PostCard.jsx
@@ -6,6 +6,7 @@ import PropTypes from 'prop-types';
 // components
 import PostImages from './PostImages';
 import CommentForm from './CommentForm';
+import PostCardContent from './PostCardContent';
 
 // antd
 import { Card, Popover, Button, Avatar, List, Comment } from 'antd';
@@ -60,7 +61,7 @@ const PostCard = ({ post }) => {
         <Card.Meta
           avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
           title={post.User.nickname}
-          description={post.content}
+          description={<PostCardContent postData={post.content} />}
         />
       </Card>
       {commentFormOpened && (
