cfe7d06bdfeb5dbb3870139ad6df171ec3149f9b
sunmin <sunm309@naver.com>
2022-01-10 14:49:15 +0900

    ♻️ 불필요한 코드 제거

--- react-next/client/redux/reducers/user.jsx
+++ react-next/client/redux/reducers/user.jsx
@@ -18,41 +18,6 @@ export const logout = () => {
   };
 };
 
-export const loginAction = (data) => {
-  return (dispatch, getState) => {
-    dispatch(loginRequestAction());
-    axios
-      .post('/api/login')
-      .then((res) => {
-        dispatch(loginSuccessAction(res.data));
-      })
-      .catch((err) => {
-        dispatch(loginFailureAction(err));
-      });
-  };
-};
-
-export const loginRequestAction = (data) => {
-  return {
-    type: 'LOG_IN_REQUEST',
-    data,
-  };
-};
-
-export const loginSuccessAction = (data) => {
-  return {
-    type: 'LOG_IN_SUCCESS',
-    data,
-  };
-};
-
-export const loginFailureAction = (data) => {
-  return {
-    type: 'LOG_IN_FAILURE',
-    data,
-  };
-};
-
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'LOG_IN':
