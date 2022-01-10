7ca70fef0484e325b4f412f233c4359c8177e7bc
sunmin <sunm309@naver.com>
2022-01-10 14:49:30 +0900

    🔨 redux-saga 설정

--- react-next/client/redux/configureStore.jsx
+++ react-next/client/redux/configureStore.jsx
@@ -1,9 +1,10 @@
 import { createWrapper } from 'next-redux-wrapper';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import thunkMiddleware from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 import reducer from './reducers';
+import rootSaga from './sagas';
 
 const loggerMiddleware =
   ({ dispatch, getState }) =>
@@ -14,12 +15,14 @@ const loggerMiddleware =
   };
 
 const configureStore = () => {
-  const middlewares = [thunkMiddleware, loggerMiddleware];
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = [sagaMiddleware, loggerMiddleware];
   const enhancer =
     process.env.NODE_ENV === 'production'
       ? compose(applyMiddleware(...middlewares))
       : composeWithDevTools(applyMiddleware(...middlewares));
   const store = createStore(reducer, enhancer);
+  store.sagaTask = sagaMiddleware.run(rootSaga);
   return store;
 };
 
