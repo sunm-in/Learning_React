2cdaabc9022f0e50ec60bc316346eb0d46ed61ee
sunmin <sunm309@naver.com>
2022-01-11 11:38:54 +0900

    🔨 user 수정 - reducer

--- react-next/client/redux/reducers/user.jsx
+++ react-next/client/redux/reducers/user.jsx
@@ -1,36 +1,62 @@
 export const initialState = {
+  isLoggingIn: false,
   isLoggedIn: false,
+  isLoggingOut: false,
   me: null,
   signUpData: {},
   loginData: {},
 };
 
-export const login = (data) => {
+export const loginRequestAction = (data) => {
   return {
-    type: 'LOG_IN',
+    type: 'LOG_IN_REQUEST',
     data,
   };
 };
 
-export const logout = () => {
+export const logoutRequestAction = () => {
   return {
-    type: 'LOG_OUT',
+    type: 'LOG_OUT_REQUEST',
   };
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'LOG_IN':
+    case 'LOG_IN_REQUEST':
       return {
         ...state,
-        isLoggedIn: true,
-        me: action.data,
+        isLoggingIn: true,
       };
-    case 'LOG_OUT':
+    case 'LOG_IN_SUCCESS':
       return {
         ...state,
+        isLoggingIn: false,
+        isLoggedIn: true,
+        me: { ...action.data, nickname: 'lsm' },
+      };
+    case 'LOG_IN_FAILURE':
+      return {
+        ...state,
+        isLoggingIn: false,
+        isLoggedIn: false,
+      };
+    case 'LOG_OUT_REQUEST':
+      return {
+        ...state,
+        isLoggingOut: true,
+        me: null,
+      };
+    case 'LOG_OUT_SUCCESS':
+      return {
+        ...state,
+        isLoggingOut: false,
         isLoggedIn: false,
         me: null,
+      };
+    case 'LOG_OUT_FAILURE':
+      return {
+        ...state,
+        isLoggingOut: false,
       };
     default:
       return state;
