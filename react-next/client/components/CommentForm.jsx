b4dfc1ada33df5e3ac998db8df8c3dbdc64c188e
sunmin <sunm309@naver.com>
2022-01-14 21:24:22 +0900

    💄 Button loading 추가

--- react-next/client/components/CommentForm.jsx
+++ react-next/client/components/CommentForm.jsx
@@ -15,7 +15,7 @@ import { ADD_COMMENT_REQUEST } from '../redux/reducers/post';
 const CommentForm = ({ post }) => {
   const dispatch = useDispatch();
   const id = useSelector((state) => state.user.me?.id);
-  const { addCommentDone } = useSelector((state) => state.post);
+  const { addCommentDone, addCommentLoading } = useSelector((state) => state.post);
   const [commentText, onChangeCommentText, setCommentText] = useInput('');
 
   useEffect(() => {
@@ -35,7 +35,12 @@ const CommentForm = ({ post }) => {
     <Form onFinish={onSubmitComment}>
       <Form.Item style={{ position: 'relative', margin: 0 }}>
         <Input.TextArea value={commentText} onChange={onChangeCommentText} rows={4} />
-        <Button style={{ position: 'absolute', right: 0, bottom: -40 }} type="primary" htmlType="submit">
+        <Button
+          style={{ position: 'absolute', right: 0, bottom: -40, zIndex: 1 }}
+          type="primary"
+          htmlType="submit"
+          loading={addCommentLoading}
+        >
           작성
         </Button>
       </Form.Item>
