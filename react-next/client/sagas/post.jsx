43c7710cf4e4b38c903d1506d4bbd1793748a65d
sunmin <sunm309@naver.com>
2022-01-16 01:04:47 +0900

    🔨 removePost saga 작성

--- react-next/client/sagas/post.jsx
+++ react-next/client/sagas/post.jsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import shortId from 'shortid';
 import { all, fork, takeLatest, delay, put, call } from 'redux-saga/effects';
 import {
   ADD_POST_REQUEST,
@@ -7,7 +8,11 @@ import {
   ADD_COMMENT_REQUEST,
   ADD_COMMENT_SUCCESS,
   ADD_COMMENT_FAILURE,
+  REMOVE_POST_REQUEST,
+  REMOVE_POST_SUCCESS,
+  REMOVE_POST_FAILURE,
 } from '../redux/reducers/post';
+import { ADD_POST_TO_ME, REMOVE_POST_OF_ME } from '../redux/reducers/user';
 
 // 게시물
 function addPostAPI(data) {
@@ -18,15 +23,47 @@ function* addPost(action) {
   try {
     // const result = yield call(addPostAPI, action.data);
     yield delay(1000);
+    const id = shortId.generate();
     yield put({
       type: ADD_POST_SUCCESS,
-      data: action.data,
+      data: {
+        id,
+        content: action.data,
+      },
+    });
+    yield put({
+      type: ADD_POST_TO_ME,
+      data: id,
     });
   } catch (err) {
     console.error(err);
     yield put({
       type: ADD_POST_FAILURE,
       error: err.response.data,
+    });
+  }
+}
+
+function removePostAPI(data) {
+  return axios.delete('/api/post', data);
+}
+
+function* removePost(action) {
+  try {
+    // const result = yield call(removePostAPI, action.data);
+    yield delay(1000);
+    yield put({
+      type: REMOVE_POST_SUCCESS,
+      data: action.data,
+    });
+    yield put({
+      type: REMOVE_POST_OF_ME,
+      data: action.data,
+    });
+  } catch (err) {
+    yield put({
+      type: REMOVE_POST_FAILURE,
+      data: err.response.data,
     });
   }
 }
@@ -56,10 +93,14 @@ function* watchAddPost() {
   yield takeLatest(ADD_POST_REQUEST, addPost);
 }
 
+function* watchRemovePost() {
+  yield takeLatest(REMOVE_POST_REQUEST, removePost);
+}
+
 function* watchAddComment() {
   yield takeLatest(ADD_COMMENT_REQUEST, addComment);
 }
 
 export default function* postSaga() {
-  yield all([fork(watchAddPost), fork(watchAddComment)]);
+  yield all([fork(watchAddPost), fork(watchRemovePost), fork(watchAddComment)]);
 }
