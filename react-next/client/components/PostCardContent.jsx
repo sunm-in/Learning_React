1dc5c7b75e6cc39102a89f06e42540387d2e52c3
sunmin <sunm309@naver.com>
2022-01-18 20:21:36 +0900

    ♻️ split 에러 해결

--- react-next/client/components/PostCardContent.jsx
+++ react-next/client/components/PostCardContent.jsx
@@ -6,16 +6,19 @@ import Link from 'next/link';
 const PostCardContent = ({ postData }) => {
   return (
     <div>
-      {postData.split(/(#[^\s#]+)/g).map((v) => {
-        if (v.match(/(#[^\s#]+)/)) {
-          return (
-            <Link href={`/hashtag/${v.slice(1)}`} key={v}>
-              <a>{v}</a>
-            </Link>
-          );
-        }
-        return v;
-      })}
+      {postData
+        .toString()
+        .split(/(#[^\s#]+)/g)
+        .map((v) => {
+          if (v.match(/(#[^\s#]+)/)) {
+            return (
+              <Link href={{ pathname: '/hashtag', query: { tag: v.slice(1) } }} key={v}>
+                <a>{v}</a>
+              </Link>
+            );
+          }
+          return v;
+        })}
     </div>
   );
 };
