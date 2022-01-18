9c1beb346d206730ddd48369d8910d5dc3f1ff7d
sunmin <sunm309@naver.com>
2022-01-18 20:19:56 +0900

    🔨 로그인 체크

--- react-next/client/pages/profile.jsx
+++ react-next/client/pages/profile.jsx
@@ -1,7 +1,8 @@
 // library
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Head from 'next/head';
+import Router from 'next/router';
 
 // components
 import AppLayout from '../components/AppLayout';
@@ -10,6 +11,15 @@ import FollowList from '../components/FollowList';
 
 const Profile = () => {
   const { me } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (!me && me.id) {
+      Router.push('/');
+    }
+  }, [me && me.id]);
+  if (!me) {
+    return null;
+  }
 
   return (
     <>
