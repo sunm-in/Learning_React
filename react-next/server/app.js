00f0e3736b05a1551d3fb71e4af2356f1cf69a40
sunmin <sunm309@naver.com>
2022-01-21 16:26:08 +0900

    Refactor: post 라우터 분리

--- react-next/server/app.js
+++ react-next/server/app.js
@@ -1,10 +1,25 @@
 const exporess = require('express');
+const postRouter = require('./routes/post');
 
 const app = exporess();
 
 app.get('/', (req, res) => {
   res.send('server test');
 });
+
+app.get('/', (req, res) => {
+  res.send('hello api');
+});
+
+app.get('/posts', (req, res) => {
+  res.json([
+    { id: 1, content: 'hello' },
+    { id: 2, content: 'hello2' },
+    { id: 3, content: 'hello3' },
+  ]);
+});
+
+app.use('/post', postRouter);
 
 app.listen(8080, () => {
   console.log('서버 실행 중');
