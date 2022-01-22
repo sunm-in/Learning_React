feaf7eb2a8f3e3ee27cad2fe6c139377ca853660
sunmin <sunm309@naver.com>
2022-01-22 09:58:02 +0900

    Feat: 추가 User model

---
+++ react-next/server/models/user.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      email: {
+        type: DataTypes.STRING(30),
+        allowNull: false,
+        unique: true,
+      },
+      nickname: {
+        type: DataTypes.STRING(30),
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+    },
+    {
+      charset: 'utf8',
+      collate: 'utf8_general_ci',
+    }
+  );
+  User.associate = (db) => {
+    db.User.hasMany(db.Post);
+    db.User.hasMany(db.Comment);
+  };
+  return User;
+};
