cdf9b948ddc68d245f739d3e8239d07b77c98cd3
sunmin <sunm309@naver.com>
2022-01-25 15:27:46 +0900

    Fix: charset error 해결

--- react-next/server/models/comment.js
+++ react-next/server/models/comment.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       charset: 'utf8mb4',
-      collate: 'utf8_general_ci',
+      collate: 'utf8mb4_general_ci',
     }
   );
   Comment.associate = (db) => {
