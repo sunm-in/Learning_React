ca6dece334921098e8c69b31796a172de3c215b8
sunmin <sunm309@naver.com>
2022-01-29 22:01:22 +0900

    feat: cors

--- react-next/server/app.js
+++ react-next/server/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const postRouter = require('./routes/post');
 const userRouter = require('./routes/user');
 const db = require('./models');
@@ -11,6 +12,11 @@ db.sequelize
   })
   .catch(console.error);
 
+app.use(
+  cors({
+    origin: '*',
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
