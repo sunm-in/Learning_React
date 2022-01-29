3c87a2efea1129bf06efa993dd324c75abb94a86
sunmin <sunm309@naver.com>
2022-01-29 23:01:09 +0900

    fix: api 주소 수정

--- react-next/client/sagas/user.jsx
+++ react-next/client/sagas/user.jsx
@@ -19,16 +19,15 @@ import {
 } from '../redux/reducers/user';
 
 function logInAPI(data) {
-  return axios.post('api/login', data);
+  return axios.post('/user/login', data);
 }
 
 function* logIn(action) {
   try {
-    // const result = yield call(logInAPI, action.data);
-    yield delay(1000);
+    const result = yield call(logInAPI, action.data);
     yield put({
       type: LOG_IN_SUCCESS,
-      data: action.data,
+      data: result.data,
     });
   } catch (err) {
     console.error(err);
@@ -40,7 +39,7 @@ function* logIn(action) {
 }
 
 function logOutAPI() {
-  return axios.post('/api/logout');
+  return axios.post('/user/logout');
 }
 
 function* logOut() {
@@ -60,7 +59,7 @@ function* logOut() {
 }
 
 function signUpAPI(data) {
-  return axios.post('http://localhost:8080/user', data);
+  return axios.post('/user', data);
 }
 
 function* signUp(action) {
