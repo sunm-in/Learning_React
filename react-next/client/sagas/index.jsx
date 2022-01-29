33cc863508542f79d2d3aab48c86fe9f6914eef5
sunmin <sunm309@naver.com>
2022-01-29 23:00:49 +0900

    feat: 임시 baseURL 추가

--- react-next/client/sagas/index.jsx
+++ react-next/client/sagas/index.jsx
@@ -1,4 +1,7 @@
 import { all, fork } from 'redux-saga/effects';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:8080';
 
 import userSaga from './user';
 import postSaga from './post';
