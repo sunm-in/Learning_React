da157d32dd5c9f85642658b5d86cc1d06f08218f
sunmin <sunm309@naver.com>
2022-01-29 22:03:20 +0900

    feat: 수정

--- react-next/client/redux/configureStore.jsx
+++ react-next/client/redux/configureStore.jsx
@@ -6,7 +6,8 @@ import createSagaMiddleware from 'redux-saga';
 import reducer from './reducers';
 import rootSaga from '../sagas';
 
-const configureStore = () => {
+const configureStore = (context) => {
+  console.log(context);
   const sagaMiddleware = createSagaMiddleware();
   const middlewares = [sagaMiddleware];
   const enhancer =
