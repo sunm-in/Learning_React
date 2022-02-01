fd226e3cc5d1ca07b1fb814e466d0689fc3f388f
sunmin <sunm309@naver.com>
2022-02-01 20:31:22 +0900

    feat: passport

--- react-next/server/passport/index.js
+++ react-next/server/passport/index.js
@@ -1,10 +1,21 @@
 const passport = require('passport');
 const local = require('./local');
+const { User } = require('../models');
 
 module.exports = () => {
-  passport.serializeUser(() => {});
+  passport.serializeUser((user, done) => {
+    done(null, user.id); // 첫 번째 인자는 서버, 두 번째는 성공
+  });
 
-  passport.deserializeUser(() => {});
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findOne({ where: { id } });
+      done(null, user); // req.user
+    } catch (error) {
+      console.error(error);
+      done(error);
+    }
+  });
 
   local();
 };
