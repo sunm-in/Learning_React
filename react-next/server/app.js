6709db9b888d4c803e5e3abbfdeb219e5763c4d7
sunmin <sunm309@naver.com>
2022-02-01 20:32:01 +0900

    feat: session, cookieParser, passport, dotenv

--- react-next/server/app.js
+++ react-next/server/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
+const dotenv = require('dotenv');
 
 const postRouter = require('./routes/post');
 const userRouter = require('./routes/user');
 const db = require('./models');
 const passportConfig = require('./passport');
 
+dotenv.config();
 const app = express();
 db.sequelize
   .sync()
@@ -23,6 +28,16 @@ app.use(
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser('secretcode'));
+app.use(
+  session({
+    saveUninitialized: false,
+    resave: false,
+    secret: process.env.COOKIE_SECRET,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
   res.send('server test');
