6458105d8839a6ec1956cbebcc22bb2eb356ff84
sunmin <sunm309@naver.com>
2024-01-23 21:55:26 +0900

    fix:  로그인 기본 폼 추가

---
+++ s-com/src/app/(beforeLogin)/@modal/page.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+const Page = () => {
+  const [id, setId] = useState();
+  const [password, setPassword] = useState();
+  const [message, setMessage] = useState();
+
+  const onSubmit = () => {};
+
+  const onClickClose = () => {};
+
+  const onChangeId = () => {};
+
+  const onChangePassword = () => {};
+
+  return <div></div>;
+};
+
+export default Page;
